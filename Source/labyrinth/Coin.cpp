3ec94dca6cfb591ed3cd064d9fc845f51746f182
Ana Lucia Blanco <analuciablanco@outlook.com>
2021-02-22 23:13:12 -0700

    minor fix

--- Source/labyrinth/Coin.cpp
+++ Source/labyrinth/Coin.cpp
@@ -29,3 +29,5 @@ int ACoin::GetPoints()
 {
 	return points;
 }
+
+// XD
--- Source/labyrinth/labyrinth.cpp
+++ Source/labyrinth/labyrinth.cpp
@@ -4,3 +4,5 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, labyrinth, "labyrinth" );
+
+// Había una vez un barco chiquito, había una vez un chiquitito barco.
